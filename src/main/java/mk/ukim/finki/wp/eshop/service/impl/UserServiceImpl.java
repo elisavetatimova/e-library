00ca76818e65b44b83f15df6b323ad77e69dd73b
ristes <ristes@gmail.com>
2020-12-17 09:21:57 +0100

    JUnit test example

--- src/main/java/mk/ukim/finki/wp/eshop/service/impl/UserServiceImpl.java
+++ src/main/java/mk/ukim/finki/wp/eshop/service/impl/UserServiceImpl.java
@@ -3,6 +3,7 @@ package mk.ukim.finki.wp.eshop.service.impl;
 import mk.ukim.finki.wp.eshop.model.Role;
 import mk.ukim.finki.wp.eshop.model.User;
 import mk.ukim.finki.wp.eshop.model.exceptions.InvalidArgumentsException;
+import mk.ukim.finki.wp.eshop.model.exceptions.InvalidUsernameOrPasswordException;
 import mk.ukim.finki.wp.eshop.model.exceptions.PasswordsDoNotMatchException;
 import mk.ukim.finki.wp.eshop.model.exceptions.UsernameAlreadyExistsException;
 import mk.ukim.finki.wp.eshop.repository.jpa.UserRepository;
@@ -32,7 +33,7 @@ public class UserServiceImpl implements UserService {
     @Override
     public User register(String username, String password, String repeatPassword, String name, String surname, Role userRole) {
         if (username==null || username.isEmpty()  || password==null || password.isEmpty())
-            throw new InvalidArgumentsException();
+            throw new InvalidUsernameOrPasswordException();
         if (!password.equals(repeatPassword))
             throw new PasswordsDoNotMatchException();
         if(this.userRepository.findByUsername(username).isPresent())
